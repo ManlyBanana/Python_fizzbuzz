81dcd74c94a92769216131b2f7a0dc6b4a2b19e3
Eshan <eshanwells@gmail.com>
2021-10-15 23:16:29 +0100

    first commit

---
+++ fizzbuzz.py
@@ -0,0 +1,24 @@
+import os
+import csv
+
+maxFor = 1000000
+
+counter = 0
+
+f = open(str(os.getcwd()) + "\\bigOutput.csv", "w")
+
+for counter in range(0,maxFor+1):
+    output = ""
+    if counter % 3 == 0:
+        output = output + "Fizz"
+    if counter % 5 == 0:
+        output = output + "Buzz"
+    if counter % 7 == 0:
+        output = output + "Bonk"
+    if counter % 7 != 0 and counter % 5 != 0 and counter % 3 != 0:
+        output = str(counter)
+    print(output)
+    writer = csv.writer(f)
+    writer.writerow(output)
+
+f.close()
